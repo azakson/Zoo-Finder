299a5a865297a346f5a1fd2183f1bc827a80a85f
Avery Zakson <avery.zakson@gmail.com>
2021-04-15 10:45:35 -0500

    Initial Commit

---
+++ Zoo Finder/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zoo Finder
+//
+//  Created by Avery Zakson on 4/15/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zoo Finder/Zoo_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zoo_FinderApp.swift
+//  Zoo Finder
+//
+//  Created by Avery Zakson on 4/15/21.
+//
+
+import SwiftUI
+
+@main
+struct Zoo_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
