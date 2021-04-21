7c905b41f662edb3d91be784099d22a4914f835a
Avery Zakson <avery.zakson@gmail.com>
2021-04-21 11:01:20 -0500

    Added User Location (I think)

--- Zoo Finder/ContentView.swift
+++ Zoo Finder/ContentView.swift
@@ -12,23 +12,17 @@ struct ContentView: View {
     @StateObject var locationManager = LocationManager()
     
     @State private var region = MKCoordinateRegion(
-
-            center: CLLocationCoordinate2D(
-
-                latitude: 41.8781,
-
-                longitude: -87.6298),
-
-            span: MKCoordinateSpan(
-
-                latitudeDelta: 0.05,
-
-                longitudeDelta: 0.05)
-
-        )
+        center: CLLocationCoordinate2D(
+            latitude: 41.8781, longitude: -87.6298),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05, longitudeDelta: 0.05))
+    @State private var userTrackingMode: MapUserTrackingMode = .follow
+    
     var body: some View {
         Map(
-            coordinateRegion: $region
+            coordinateRegion: $region,
+            showsUserLocation: true,
+            userTrackingMode: $userTrackingMode
         )
     }
 }
@@ -41,15 +35,10 @@ struct ContentView_Previews: PreviewProvider {
 
 struct Marker: View {
     var mapItem: MKMapItem
-
     var body: some View {
-
         if let url = mapItem.url {
-
             Link(destination: url, label: {
-
                 Image("monke")
-
             })
         }
     }
