09197596c6d4e60084a60311f7594a70a989c780
Matt Imdad <jjimdad@gmail.com>
2021-04-21 10:39:26 -0500

    added location manager

---
+++ Zoo Finder/LocationManager.swift
@@ -0,0 +1,26 @@
+//
+//  LocationManager.swift
+//  Zoo Finder
+//
+//  Created by Student on 4/21/21.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    override init() {
+
+           super.init()
+
+           locationManager.delegate = self
+
+           locationManager.requestWhenInUseAuthorization()
+
+           locationManager.startUpdatingLocation()
+
+       }
+
+    var locationManager = CLLocationManager()
+    
+}
